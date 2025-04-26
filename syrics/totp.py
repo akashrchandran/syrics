3d23b4954d76fc10aaaf84a6c31a1a0357ecc278
Akash R Chandran <chandranrakash@gmail.com>
2025-04-26 10:58:22 +0530

    Fix spotify login

---
+++ syrics/totp.py
@@ -0,0 +1,28 @@
+import hashlib
+import hmac
+import math
+
+# thanks to https://github.com/glomatico/votify/blob/main/votify/totp.py
+class TOTP:
+    def __init__(self) -> None:
+        self.secret = b"5507145853487499592248630329347"
+        self.version = 5
+        self.period = 30
+        self.digits = 6
+
+    def generate(self, timestamp: int) -> str:
+        counter = math.floor(timestamp / 1000 / self.period)
+        counter_bytes = counter.to_bytes(8, byteorder="big")
+
+        h = hmac.new(self.secret, counter_bytes, hashlib.sha1)
+        hmac_result = h.digest()
+
+        offset = hmac_result[-1] & 0x0F
+        binary = (
+            (hmac_result[offset] & 0x7F) << 24
+            | (hmac_result[offset + 1] & 0xFF) << 16
+            | (hmac_result[offset + 2] & 0xFF) << 8
+            | (hmac_result[offset + 3] & 0xFF)
+        )
+
+        return str(binary % (10**self.digits)).zfill(self.digits)
