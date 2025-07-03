03cd0bd67e3214e4d752656e1fcf7679c47bc0f9
Akash R Chandran <chandranrakash@gmail.com>
2025-07-03 17:05:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name=pkg_name,
-    version="0.1.2.0",
+    version="0.1.2.1",
     author="Akash R Chandran",
     author_email="chandranrakash@gmail.com",
     description="A command line tool to fetch lyrics from spotify and save it to lrc file. It can fetch both synced and unsynced lyrics from spotify. ",
